ce938b9283d99108b3314c3906594e308a69065f
Vollga <jancarlpeters@gmail.com>
2019-03-27 17:48:59 +0100

    Progress bars

---
+++ Assets/Scripts/ProgressBar.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class ProgressBar : MonoBehaviour
+{
+    public float progressTime;
+
+    Image barImage;
+    float fill = 0f;
+    float timeStart;
+    float timeEnd;
+   
+    // Start is called before the first frame update
+    void Start()
+    {
+        barImage = gameObject.GetComponent<Image>();
+        timeStart = Time.unscaledTime;
+        timeEnd = Time.unscaledTime + progressTime;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        fill = (Time.unscaledTime - timeStart) / timeEnd;
+        barImage.fillAmount = fill;
+        if (fill > 1.1f)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
